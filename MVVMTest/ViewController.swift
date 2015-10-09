b6306fdaab890f5714abfd1c5dafc2e7d6620f4b
willowcheng <e.wingus@Gmail.com>
2015-10-09 15:28:48 -0400

    Add UI and outlets in view controller

--- MVVMTest/ViewController.swift
+++ MVVMTest/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
