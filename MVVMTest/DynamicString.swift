ee3c2411b639d6ad9e693931c552b551492058fe
willowcheng <e.wingus@Gmail.com>
2015-10-14 11:01:23 -0400

    Try binding initially

---
+++ MVVMTest/DynamicString.swift
@@ -0,0 +1,19 @@
+//
+//  DynamicString.swift
+//  MVVMTest
+//
+//  Created by Liu Cheng on 2015-10-14.
+//  Copyright © 2015 Liu Cheng. All rights reserved.
+//
+
+import Foundation
+
+class DynamicString {
+    
+    var value: String
+    
+    init(_ v: String) {
+        value = v
+    }
+    
+}
